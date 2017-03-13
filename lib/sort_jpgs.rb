b4f1217aae19912d9d0be9e85614578d39afa8c3
joh-mue <yesiamkeen@gmail.com>
2017-03-13 12:31:44 +0100

    turned script into module and sorter class

--- lib/sort_jpgs.rb
+++ lib/sort_jpgs.rb
@@ -1,127 +1,99 @@
-#!/usr/bin/ruby
 # frozen_string_literal: true
 
 require 'exifr'
 require 'fileutils'
 require 'date'
-require 'optparse'
 require 'logger'
 require 'schlib/spinner'
 
-FORMAT = '%y-%m-%d %H:%M:%S'
+module SortJPGS
+  FORMAT = '%y-%m-%d %H:%M:%S'
 
-if __FILE__ == $PROGRAM_NAME
-  Log = Logger.new("sort_jpgs_#{Time.now.to_i}.log")
-  opts = parse_options
-  stats = new Hash(0)
+  # This is where all the magic happens
+  class Sorter
+    def initialize(opts)
+      @source = opts[:source]
+      @output = opts[:output]
+      @move = opts[:move]
+      @threshold = opts[:threshold]
+    end
 
-  files = Dir.glob File.join(source_path, '*/*.jpg')
-  Log.info(time) { "Log file for sort_jpgs from #{Time.now} \n #{files.count} files where found." }
+    # rubocop:disable Metrics/AbcSize
+    def run
+      log = Logger.new("sort_jpgs_#{Time.now.to_i}.log")
+      stats = Hash.new(0)
 
-  Schlib::Spinner.wait_for do
-    files.select { |file| File.size(file) >= opts[:threshold] }.each do |file|
-      begin
-        pic = EXIFR::JPEG.new(file)
-        target_dir = create_target_dir(pic, opts[:output])
-        filename = create_filename(pic, target_dir)
-        handle_file(opts[:move], file, target_dir, filename)
-        update_statistics(stats, pic.model)
-      rescue EXIFR::MalformedJPEG => e
-        Log.error { "EXIFR::MalformedJPEG exception was raised while handling #{file}.\n#{e}" }
+      files = Dir.glob File.join(@source, '*/*.jpg')
+      log.info(time) { "SortJPGS log from #{Time.now} \n #{files.count} files where found." }
+
+      Schlib::Spinner.wait_for do
+        files.select { |file| File.size(file) >= @threshold }.each do |file|
+          begin
+            pic = EXIFR::JPEG.new(file)
+            target_dir = create_target_dir(pic)
+            filename = create_filename(pic, target_dir)
+            handle_file(file, target_dir, filename)
+            update_statistics(stats, pic.model)
+          rescue EXIFR::MalformedJPEG => e
+            log.error { "EXIFR::MalformedJPEG exception was raised while handling #{file}.\n#{e}" }
+          end
+        end
+      end
+
+      log.info(time) { "#{stats[:moved_or_copied]} files were moved/copied." }
+      log.info(time) { stats }
+    end
+    # rubocop:enable Metrcis/AbcSize
+
+    def time
+      Time.now.strftime(FORMAT)
+    end
+
+    # create directories if nonexistant
+    def create_target_dir(pic)
+      year = pic.date_time.to_s[0, 4] # e.g. "2013"
+      month = pic.date_time.to_s[5, 2] # 08
+      day = pic.date_time.to_s[8, 2] # 2012-08-13
+      target_dir = File.join(@output, pic.model, year, month, day)
+      FileUtils.makedirs target_dir unless Dir.exist?(target_dir)
+      target_dir
+    end
+
+    # check if file already exists and create appropriate filename
+    def create_filename(pic, target_dir)
+      existing_files = Dir.glob File.join(target_dir, "#{pic.date_time.to_i}*")
+      if existing_files.empty?
+        pic.date_time.to_i.to_s + '.jpg'
+      else
+        increment_filename(existing_files)
       end
     end
-  end
 
-  Log.info(time) { "#{stats[:moved_or_copied]} files were moved/copied." }
-  Log.info(time) { stats }
-end
-
-def time
-  Time.now.strftime(FORMAT)
-end
-
-# create directories if nonexistant
-def create_target_dir(pic, output)
-  year = pic.date_time.to_s[0, 4] # e.g. "2013"
-  month = pic.date_time.to_s[5, 2] # 08
-  day = pic.date_time.to_s[8, 2] # 2012-08-13
-  target_dir = File.join(output, pic.model, year, month, day)
-  FileUtils.makedirs target_dir unless Dir.exist?(target_dir)
-  target_dir
-end
-
-# check if file already exists and create appropriate filename
-def create_filename(pic, target_dir)
-  existing_files = Dir.glob File.join(target_dir, "#{pic.date_time.to_i}*")
-  if existing_files.empty?
-    pic.date_time.to_i.to_s + '.jpg'
-  else
-    increment_filename(existing_files)
-  end
-end
-
-def increment_filename(existing_files)
-  base = File.basename(existing_files.first, '.jpg').split('-').first
-  count = 1
-  if existing_files.size > 1
-    count = existing_files.map { |file| File.basename(file, '.jpg') }
-                          .select { |name| name.include? '-' }
-                          .map { |name| name.split('-').last.to_i }
-                          .sort
-                          .last
-                          .succ
-  end
-  "#{base}-#{count}.jpg"
-end
-
-def handle_file(move, file, target_dir, filename)
-  if move
-    FileUtils.move(file, File.join(target_dir, filename))
-  else
-    FileUtils.copy(file, File.join(target_dir, filename))
-  end
-end
-
-def update_statistics(statistics, camera_model)
-  statistics[camera_model] += 1
-  statistics[:moved_or_copied] += 1
-end
-
-# rubocop:disable Metrics/MethodLength
-def parse_options
-  opts = { source: './', output: './', move: false }
-
-  optparse = OptionParser.new do |parser|
-    parser.banner = "Usage: \bsort_jpgs [-h] [-s SOURCE_PATH] -o OUTPUT_PATH\n\n"
-
-    ### options and what they do
-    parser.on('-s', '--source-path DIR',
-              'Set the directory that contains files to be sorted. Default is PWD') do |source|
-      opts[:source] = source
+    def increment_filename(existing_files)
+      base = File.basename(existing_files.first, '.jpg').split('-').first
+      count = 1
+      if existing_files.size > 1
+        count = existing_files.map { |file| File.basename(file, '.jpg') }
+                              .select { |name| name.include? '-' }
+                              .map { |name| name.split('-').last.to_i }
+                              .sort
+                              .last
+                              .succ
+      end
+      "#{base}-#{count}.jpg"
     end
 
-    parser.on('-o', '--output-path DIR',
-              'Set the directory that the files will be written to.') do |output|
-      opts[:output] = output
+    def handle_file(file, target_dir, filename)
+      if @move
+        FileUtils.move(file, File.join(target_dir, filename))
+      else
+        FileUtils.copy(file, File.join(target_dir, filename))
+      end
     end
 
-    parser.on('-m', '--move', 'Move files instead of copying them.') do
-      opts[:move] = true
-    end
-
-    parser.on('-t', '--threshold VALUE', Integer,
-              'Set the minimum size that a picture has to be in kB.') do |threshold|
-      opts[:threshold] = threshold.to_i * 1000
-    end
-
-    # This displays the help screen
-    parser.on_tail('-h', '--help', 'Display this screen.') do
-      puts parser
-      exit
+    def update_statistics(statistics, camera_model)
+      statistics[camera_model] += 1
+      statistics[:moved_or_copied] += 1
     end
   end
-
-  optparse.parse!
-  opts
 end
-# rubocop:enable Metrics/MethodLength
--- spec/sort_jpgs_spec.rb
+++ spec/sort_jpgs_spec.rb
@@ -2,55 +2,76 @@
 require 'spec_helper'
 
 describe SortJpgs do
+  describe 'tests' do
+    it 'can access methods' do
+      opts = { source: './', output: './output', move: false }
+      sorter = SortJPGS::Sorter.new(opts)
+      puts sorter.time
+    end
+  end
+
   describe '#increment_filename' do
+    before(:all) do
+      opts = { source: './', output: './output', move: false }
+      @sorter = SortJPGS::Sorter.new(opts)
+    end
+
     it 'adds -1 if file exists' do
       files = ['/folder/12439870.jpg']
-      expect(increment_filename(files)).to eq('12439870-1.jpg')
+      expect(@sorter.increment_filename(files)).to eq('12439870-1.jpg')
     end
 
     it 'increments properly' do
       files = ['/folder/12439870.jpg', '/folder/12439870-1.jpg']
-      expect(increment_filename(files)).to eq('12439870-2.jpg')
+      expect(@sorter.increment_filename(files)).to eq('12439870-2.jpg')
     end
 
     it 'increments from 9 to 10' do
       files = ['/folder/12439870.jpg']
       (1..9).each { |i| files.push "/folder/12439870-#{i}.jpg" }
-      expect(increment_filename(files)).to eq('12439870-10.jpg')
+      expect(@sorter.increment_filename(files)).to eq('12439870-10.jpg')
     end
 
     it 'increments into double digits' do
       files = ['/folder/12439870.jpg']
       (1..10).each { |i| files.push "/folder/12439870-#{i}.jpg" }
-      expect(increment_filename(files)).to eq('12439870-11.jpg')
+      expect(@sorter.increment_filename(files)).to eq('12439870-11.jpg')
     end
 
     it 'increments even if first file is not there' do
       files = []
       (1..3).each { |i| files.push "/folder/12439870-#{i}.jpg" }
-      expect(increment_filename(files)).to eq('12439870-4.jpg')
+      expect(@sorter.increment_filename(files)).to eq('12439870-4.jpg')
+    end
+
+    after(:all) do
+      @sorter = nil
     end
   end
 
   describe '#create_target_dir' do
-    test_dir = 'spec/tests/'
+    test_dir = 'spec/tests/output'
+
+    before(:all) do
+      opts = { source: './', output: test_dir, move: false }
+      @sorter = SortJPGS::Sorter.new(opts)
+    end
+
     before(:each) do
       FileUtils.makedirs test_dir
     end
 
     it 'creates the target_dir' do
       dummy_pic = EXIFR::JPEG.new('spec/dummy.jpg')
-      test_output = File.join(test_dir, '/output')
-      target_dir = create_target_dir(dummy_pic, test_output)
-      expect(target_dir).to eq(File.join(test_output, 'iPhone 5', '2013', '03', '21'))
+      target_dir = @sorter.create_target_dir(dummy_pic)
+      expect(target_dir).to eq(File.join(test_dir, 'iPhone 5', '2013', '03', '21'))
     end
 
     it 'does not override existing target_dir' do
       dummy_pic = EXIFR::JPEG.new('spec/dummy.jpg')
-      test_output = File.join(test_dir, '/output')
-      target_dir = create_target_dir(dummy_pic, test_output)
+      target_dir = @sorter.create_target_dir(dummy_pic)
       creation_time = File.ctime(target_dir)
-      target_dir = create_target_dir(dummy_pic, test_output)
+      target_dir = @sorter.create_target_dir(dummy_pic)
       expect(File.ctime(target_dir)).to eq(creation_time)
     end
 
@@ -60,30 +81,36 @@ describe SortJpgs do
   end
 
   describe '#create_filename and #handle_file' do
+    @move = false
     test_dir = 'spec/tests/'
     dummy_pic = EXIFR::JPEG.new('spec/dummy.jpg')
+
+    before(:all) do
+      opts = { source: './', output: File.join(test_dir, '/output'), move: false }
+      @sorter = SortJPGS::Sorter.new(opts)
+    end
 
     before(:each) do
       FileUtils.makedirs test_dir
     end
 
     it "creates basename if it's the first of its kind" do
-      target_dir = create_target_dir(dummy_pic, File.join(test_dir, '/output'))
+      target_dir = @sorter.create_target_dir(dummy_pic)
 
-      filename = create_filename(dummy_pic, target_dir)
+      filename = @sorter.create_filename(dummy_pic, target_dir)
       expect(filename).to eq(dummy_pic.date_time.to_i.to_s + '.jpg')
     end
 
     it 'handles multiple copies without overriding one' do
-      target_dir = create_target_dir(dummy_pic, File.join(test_dir, '/output'))
+      target_dir = @sorter.create_target_dir(dummy_pic)
 
-      filename = create_filename(dummy_pic, target_dir)
-      handle_file(false, File.new('spec/dummy.jpg'), target_dir, filename)
+      filename = @sorter.create_filename(dummy_pic, target_dir)
+      @sorter.handle_file(File.new('spec/dummy.jpg'), target_dir, filename)
 
-      filename = create_filename(dummy_pic, target_dir)
+      filename = @sorter.create_filename(dummy_pic, target_dir)
       expect(filename).to eq(dummy_pic.date_time.to_i.to_s + '-1.jpg')
 
-      handle_file(false, File.new('spec/dummy.jpg'), target_dir, filename)
+      @sorter.handle_file(File.new('spec/dummy.jpg'), target_dir, filename)
       files = Dir.glob File.join(target_dir, "#{dummy_pic.date_time.to_i}*")
       expect(files.size).to eq(2)
     end
